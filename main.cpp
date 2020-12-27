f4b762ff588ef45bcc93ce0550ecec26e232da27
Kosyo Nikolov <kosyo.nikolov@protonmail.com>
2020-12-27 16:01:18 +0200

    algo + draw

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 
 #include <opencv2/opencv.hpp>
 
+// ================================================
+// ================= Boring stuff =================
+// ================================================
+
 struct ImageDesciptor
 {
     double xMin, xMax;
@@ -71,9 +75,117 @@ ImageDesciptor createDescriptor(const std::vector<cv::Point2d> & points,
     return result;
 };
 
+// ================================================
+// ================= Debug images =================
+// ================================================
+
+void drawPoints(cv::Mat & image,
+                const std::vector<cv::Point2d> & points,
+                const std::vector<cv::Point2d> & centers,
+                const std::vector<int> & clusterId,
+                const ImageDesciptor & desc)
+{
+    cv::rectangle(image,
+                  cv::Rect(0, 0, image.cols, image.rows),
+                  cv::Scalar(0, 0, 0),
+                  cv::FILLED);
+
+    std::array<cv::Scalar, 8> colors = 
+    {
+        cv::Scalar(0, 0, 255), // red
+        cv::Scalar(0, 255, 0), // green
+        cv::Scalar(255, 0, 0), // blue
+        cv::Scalar(0, 0, 127), // dark red
+        cv::Scalar(0, 127, 0), // dark green
+        cv::Scalar(127, 0, 0), // dark blue
+        cv::Scalar(0, 255, 255), // orange
+        cv::Scalar(255, 255, 0)  // purple
+    };
+
+    // ======== Draw points ========
+    for (int i = 0; i < points.size(); i++)
+    {
+        const cv::Point2i point = desc.pt2img(points[i]);
+        cv::circle(image, point, 2, colors[clusterId[i]], cv::FILLED);
+    }
+
+    // ======== Draw centers ========
+    for (int i = 0; i < centers.size(); i++)
+    {
+        const cv::Point2i point = desc.pt2img(centers[i]);
+        const cv::Scalar color  = colors[i];
+        
+        cv::line(image, 
+                 point - cv::Point2i(0, 5),
+                 point + cv::Point2i(0, 5),
+                 color);
+
+        cv::line(image, 
+                 point - cv::Point2i(5, 0),
+                 point + cv::Point2i(5, 0),
+                 color);
+    }
+}
+
+// ================================================
+// =================== Algorithm ==================
+// ================================================
+
+double sqDistance(const cv::Point2d & a, const cv::Point2d & b)
+{
+    const double dx = a.x - b.x;
+    const double dy = a.y - b.y;
+    return dx * dx + dy * dy;
+}
+
+bool classifyAndUpdate(const std::vector<cv::Point2d> & points,
+                       const std::vector<cv::Point2d> & centers,
+                       std::vector<int> & outClusterId,
+                       std::vector<cv::Point2d> & outNewCenters)
+{
+    const int clusters = centers.size();
+    for (int i = 0; i < clusters; i++) outNewCenters[i] = cv::Point2d(0, 0);
+
+    std::vector<int> count(clusters);
+
+    // ======== Assign each point to one cluster ========
+    for (int i = 0; i < points.size(); i++)
+    {
+        const auto & pt = points[i];
+        
+        int id = 0;
+        double dist = sqDistance(pt, centers[0]);
+        for (int j = 1; j < clusters; j++)
+        {
+            const double cand = sqDistance(pt, centers[j]);
+            if (cand < dist)
+            {
+                id = j;
+                dist = cand;
+            }
+        }
+
+        outClusterId[i] = id;
+        count[id]++;
+        outNewCenters[id] += pt;
+    }
+
+    // ======== Compute new cluster centers and compare with old ========
+    bool haveDifferent = false;
+    for (int i = 0; i < clusters; i++)
+    {
+        outNewCenters[i] /= count[i];
+        haveDifferent |= outNewCenters[i] != centers[i];
+    }
+
+    return haveDifferent;
+}
+
 int main(int argc, char** argv) 
 {
     const std::string USAGE_MSG = "Usage: ./kMeans [points] [cluster count]";
+
+    cv::Point2d test;
 
     if (argc != 3)
     {
